851f619d43ba75fe6d511dcd4d3c44fa126bc24a
NeilManson <n.manson@rgu.ac.uk>
2023-03-24 11:12:56 +0000

    removed old commented code

--- index.js
+++ index.js
@@ -71,50 +71,6 @@ app.get("/addQuotes", function(req, res){
   res.render("addQuotes")
 })
 
-// post paths
-
-// app.post('/doLogin',async function(req, res) {
-//   var email_log = req.body.email;
-//   var pword = req.body.password;
-//   console.log(email_log);
-//   var result = await User.findOne({email:email_log, password:pword});
-//   console.log(result)
-//   res.send("logged in");
-  
-//   // User.findOne({email: email_log, password: pword}, function (err, user) {
-//   //   if(err) {
-//   //     console.log(err);
-//   //     return res.status(500).send();
-//   //   }
-
-//   //   if(!user) {
-//   //     res.redirect("/signIn");
-//   //     return res.status(404).send()
-//   //   }
-//   //   req.session.loggedin = true;
-//   //   req.session.email = email;
-//   //   req.session.isadmin = user.isAdmin
-//   //   return res.status(200).send()
-//   // })
-// })
-
-// app.post('/doRegister',async function(req, res) {
-//   var email = req.body.email;
-//   var password = req.body.password;
-//   var firstname = req.body.firstname;
-//   var lastname = req.body.lastname;
-
-//   var newUser = new User();
-//   newUser.email = email;
-//   newUser.password = password;
-//   newUser.firstname = firstname;
-//   newUser.lastname = lastname;
-//   console.log(newUser)
-//   await newUser.save()
-//   res.redirect("/signIn");
-// })
-
-
 // 404 page
 app.use(function ( req, res, next) {
     res.send('This page does not exist!')
