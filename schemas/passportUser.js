5a3f18d163b0eda0dc2488c3d820665d7feebc3b
Your name <1712991@rgu.ac.uk>
2023-04-06 00:24:22 +0100

    added production user database to schema

--- schemas/passportUser.js
+++ schemas/passportUser.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
-const passportLocalMongoose = require('passport-local-mongoose')
+const passportLocalMongoose = require('passport-local-mongoose');
+
 
 // Create User Schema
 const UserSchema = new mongoose.Schema({
-    username:{type:String, unique:true, match:[/.+\@.+\..+/, 'Please fill a valid email address']},
-    isAdmin:{type:Boolean, default:false},
-    password:String
-})
+    username: { type: String, unique: true, match: [/.+\@.+\..+/, 'Please fill a valid email address'], required: "Email is required" },
+    isAdmin: { type: Boolean, default: false },
+    password: { type: String, }
+});
 
 // Hash password
 UserSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User-prod', UserSchema);
