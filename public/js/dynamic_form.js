5a0d435ead043028e05a29f57f4281a063742e1a
Your name <1712991@rgu.ac.uk>
2023-04-05 14:53:14 +0100

    removed log

--- public/js/dynamic_form.js
+++ public/js/dynamic_form.js
@@ -1,6 +1,5 @@
 function showDiv() {
-    console.log("working")
     const container = document.getElementById('additional-costs');
-    const section = document.getElementById('resources');  
+    const section = document.getElementById('resources');
     container.appendChild(section.cloneNode(true));
 }
