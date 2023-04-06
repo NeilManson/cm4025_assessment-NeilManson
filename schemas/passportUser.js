e3e9d9527441e2894f1386ad330739294ea7c967
Your name <1712991@rgu.ac.uk>
2023-04-06 16:06:29 +0100

    added password validation to user schema

--- schemas/passportUser.js
+++ schemas/passportUser.js
@@ -6,10 +6,18 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const UserSchema = new mongoose.Schema({
     username: { type: String, unique: true, match: [/.+\@.+\..+/, 'Please fill a valid email address'], required: "Email is required" },
     isAdmin: { type: Boolean, default: false },
-    password: { type: String, }
 });
 
+const passwordValidation = function(password, cb) {
+    const regex = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})');
+    if(!regex.test(password)){
+        return cb("the password does not meet the criteria. Please return to registration page and try a stronger password")
+    }else{
+        return cb()
+    }
+}
+
 // Hash password
-UserSchema.plugin(passportLocalMongoose);
+UserSchema.plugin(passportLocalMongoose,{passwordValidator:passwordValidation});
 
 module.exports = mongoose.model('User-prod', UserSchema);
